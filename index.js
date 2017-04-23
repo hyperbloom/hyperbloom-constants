eacbb135beef86e505863f5c251d2ed5296dba08
Fedor Indutny <fedor@indutny.com>
2017-04-23 01:35:10 -0400

    lib: remove hypercore constants

--- index.js
+++ index.js
@@ -9,6 +9,3 @@ exports.SIGNATURE_SIZE = 64;
 exports.HASH_SIZE = 32;
 exports.HASH_KEY = Buffer.from('---hyperbloom---');
 exports.MAX_CHAIN_LENGTH = 5;
-
-exports.DISCOVERY_HASH_KEY = Buffer.from('hypercore');
-exports.DISCOVERY_KEY_SIZE = 32;
